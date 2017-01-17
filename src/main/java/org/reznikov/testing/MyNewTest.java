237803695c2211bc5094aa2f7dc6324057254cd7
antonmalykhin <anton.m1989@gmail.com>
2017-01-17 13:50:42 +0200

    1. replaced Threadslip by Wait 2. added test with Meizu phone

--- src/main/java/org/reznikov/testing/MyNewTest.java
+++ src/main/java/org/reznikov/testing/MyNewTest.java
@@ -12,6 +12,9 @@ public class MyNewTest extends BaseWebDriverTest{
     public static final String INPUT_SELECTOR = "//input[contains(@class,'rz-header-search-input-text')]";
     public static final String FILTER_SELECTOR = "a.filter-active-i-link";
     public static final String IPHONE_ITEM = "//div[contains(@class,'g-i-tile g-i-tile-catalog')]//div[contains(@class,'g-i-tile-i-title')]/a[contains(text(),'Apple iPhone 7 128GB Jet Black')]";
+    public static final String MEIZU_ITEM = "//div[contains(@class,'g-i-tile g-i-tile-catalog')]//div[contains(@class,'g-i-tile-i-title')]/a[contains(text(),'Meizu M2 White (Международная версия)')]";
+    public static final String PUPCHASE_BUTTON="//button[@name='topurchases']";
+    public static final String CART_ITEM_LINK="//a[@class='novisited cart-i-title-link'][@href='http://rozetka.com.ua/meizu_m2_16gb_wh_world/p6675387/']";
 
     @Test
     public void testSite(){
@@ -48,12 +51,39 @@ public class MyNewTest extends BaseWebDriverTest{
         wait.until(ExpectedConditions.elementToBeClickable(By.name("rz-search-button")))
                 .click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(IPHONE_ITEM))).click();
-        try {
-            Thread.sleep(5000);
-        } catch (Exception e){
 
-        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PUPCHASE_BUTTON)));
+
+//        try {
+//            Thread.sleep(5000);
+//        } catch (Exception e){
+//
+//        }
+
+
         assert driver.getCurrentUrl().equals("http://rozetka.com.ua/apple_iphone_7_128gb_jet_black/p11241577/");
+
+
+    }
+
+    @Test
+    public void testRozetka2(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(INPUT_SELECTOR)))
+                .clear();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(INPUT_SELECTOR)))
+                .sendKeys("meizu");
+        wait.until(ExpectedConditions.elementToBeClickable(By.name("rz-search-button")))
+                .click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(MEIZU_ITEM))).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PUPCHASE_BUTTON)));
+
+        assert driver.getCurrentUrl().equals("http://rozetka.com.ua/meizu_m2_16gb_wh_world/p6675387/");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PUPCHASE_BUTTON))).click();
+
+        wait.until(ExpectedConditions.textToBe(By.xpath(CART_ITEM_LINK),"Meizu M2 White (Международная версия)"));
+
 
 
     }
